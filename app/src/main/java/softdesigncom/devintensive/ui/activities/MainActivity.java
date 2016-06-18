2912f537f311401898f3b126eb9e70bdd526f8e5
Reafex <s.terakopov@gmail.com>
2016-06-18 17:03:19 +0300

    Initial commit

---
+++ app/src/main/java/softdesigncom/devintensive/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package softdesigncom.devintensive.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import softdesigncom.devintensive.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
